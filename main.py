83dceaef43692c42e35ace4cdcc210518c89fa3d
Hernan <hernan@mukiku.com>
2022-02-05 09:57:13 -0300

    'add-main-and-random'

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import random
+
+
 def main():
-    pass
+    num_aleatorio = random.randint(1, 101)
+    num_usuario = int(input("Adivina el número (entre 1 y 100): "))
+    while num_usuario != num_aleatorio:
+        if num_usuario < num_aleatorio:
+            print("El número es mayor")
+        else:
+            print("El número es menor")
+        num_usuario = int(input("Adivina el número (entre 1 y 100): "))
+    print("¡Has acertado!")
 
 
 if __name__ == "__main__":
